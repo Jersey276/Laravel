ca06e6541a6a2c0d312ab50ee4bfbbab96e03337
Tristan <tristan-lefevre@hotmail.fr>
2021-09-12 21:05:48 +0200

    add flash message system + add flash message manager on controller + add bootstrap with npm = update templates for use design on local file - remove former session message system on home page

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,9 +5,27 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected function errorFlash(Request $request, String $message)
+    {
+        $request->session()->flash('status-type','danger');
+        $this->flash($request, $message);
+    }
+
+    protected function successFlash(Request $request, String $message)
+    {
+        $request->session()->flash('status-type','success');
+        $this->flash($request, $message);
+    }
+
+    private function flash(Request $request, String $message)
+    {
+        $request->session()->flash('status-message', $message);
+    }
 }
