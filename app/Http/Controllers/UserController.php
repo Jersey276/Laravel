bc3bc392846b95c33ce7a7da5eda1f7e46c7ce03
Tristan <tristan-lefevre@hotmail.fr>
2021-09-18 05:15:43 +0200

    minor fix - remove unused method / fix post policy = fix post policy - remove unused method on User controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -62,27 +62,4 @@ class UserController extends Controller
     {
         return view('/admin/users/form',['user' => User::where(['name' => $name])->first(), 'roles' => Role::all()]);
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, User $user)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(User $user)
-    {
-        //
-    }
 }
